a45a99839a8f3e4dc0d886f93408ba9ec36bff9e
www-net <78052177+www-net@users.noreply.github.com>
2024-09-16 20:32:35 +0300

    refactor: 5.1.2 transfer the same implementation of components

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -1,8 +1,23 @@
+import {createElement} from "../utils";
+
 export default class AbstractComponent {
   constructor() {
     if (new.target === AbstractComponent) {
       throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
     }
+
+    this._element = null;
+  }
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getelement());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
 
