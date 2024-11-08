2c61e90896d20aef84f61bd4843e7e22e284aef2
www-net <78052177+www-net@users.noreply.github.com>
2024-11-08 12:50:18 +0300

    feat: create model for tasks and switches the controller to model operation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import BoardComponent from './components/board';
 import BoardController from './controllers/board';
 import FilterComponent from './components/filter';
 import SiteMenuComponent from './components/site-menu';
+import TasksModel from './models/tasks';
 import {generateTasks} from './mock/task';
 import {generateFilters} from './mock/filter';
 import {render} from './utils/render';
@@ -14,11 +15,13 @@ const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
 
 render(siteHeaderElement, new SiteMenuComponent());
 render(siteMainElement, new FilterComponent(filters));
 
 const boardComponent = new BoardComponent();
-const boardController = new BoardController(boardComponent);
+const boardController = new BoardController(boardComponent, tasksModel);
 render(siteMainElement, boardComponent);
-boardController.render(tasks);
+boardController.render();
