3cd3578173712e7ee11f1908c3bf2c84a72fc876
www-net <78052177+www-net@users.noreply.github.com>
2024-11-28 19:24:01 +0300

    feat: describe the screen switching function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import BoardComponent from './components/board';
 import BoardController from './controllers/board';
 import FilterController from './controllers/filter';
+import StatisticsComponent from './components/statistics';
 import SiteMenuComponent, {MenuItem} from './components/site-menu';
 import TasksModel from './models/tasks';
 import {generateTasks} from './mock/task';
@@ -28,11 +29,31 @@ render(siteMainElement, boardComponent);
 const boardController = new BoardController(boardComponent, tasksModel);
 boardController.render();
 
+const dateTo = new Date();
+const dateFrom = (() => {
+  const d = new Date(dateTo);
+  d.setDate(d.getDate() - 7);
+  return d;
+})();
+const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
+statisticsComponent.hide();
+
 siteMenuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
     case MenuItem.NEW_TASK:
       siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
       boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticsComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
       break;
   }
 });
