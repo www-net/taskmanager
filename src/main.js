9b930a67ba84c22b5f2fef6163b5285a884042a3
www-net <78052177+www-net@users.noreply.github.com>
2024-11-29 13:07:46 +0300

    refactor: refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import BoardComponent from './components/board';
 import BoardController from './controllers/board';
 import FilterController from './controllers/filter';
-import StatisticsComponent from './components/statistics';
+
 import SiteMenuComponent, {MenuItem} from './components/site-menu';
+import StatisticsComponent from './components/statistics';
 import TasksModel from './models/tasks';
 import {generateTasks} from './mock/task';
 import {render, RenderPosition} from './utils/render';
@@ -10,32 +11,30 @@ import {render, RenderPosition} from './utils/render';
 
 const TASK_COUNT = 18;
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-const siteMenuComponent = new SiteMenuComponent();
-
-render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
-
-const tasks = generateTasks(TASK_COUNT);
-const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
-
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
-
-const boardComponent = new BoardComponent();
-render(siteMainElement, boardComponent);
-
-const boardController = new BoardController(boardComponent, tasksModel);
-boardController.render();
-
 const dateTo = new Date();
 const dateFrom = (() => {
   const d = new Date(dateTo);
   d.setDate(d.getDate() - 7);
   return d;
 })();
+
+const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuComponent();
 const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+
+const boardComponent = new BoardComponent();
+const boardController = new BoardController(boardComponent, tasksModel);
+const filterController = new FilterController(siteMainElement, tasksModel);
+
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+filterController.render();
+render(siteMainElement, boardComponent);
+boardController.render();
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 statisticsComponent.hide();
 
